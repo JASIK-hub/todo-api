1a7054ef7d50d499306097cf9af9d08a88891cf7
JASIK-hub <zhasik7002@gmail.com>
2025-02-22 11:29:16 +0500

    new file

--- script.js
+++ script.js
@@ -1,42 +1,66 @@
 const localhostAddress = "https://dummyjson.com/todos";
 const newTodoInput = document.querySelector('input');
 let submitButton = document.querySelector("#submit");
+const todosContainer = document.getElementById('todos');
 
+async function getTodos() {
+  let prom = await fetch(localhostAddress);
+  let response = await prom.json();
 
-
-
-async function getTodos(){
-  let prom=await fetch(localhostAddress)
-  let responce=await prom.json()
-  for(let i=0;i<responce.todos.length;i++){
-    document.getElementById('todos').innerHTML+=`
-  <div class="todo">
-            <span class='itemTodo'>${responce.todos[i].todo}</span>
-
-            <div class="actions">
-                <button class="edit">
-                    <i class="fas fa-edit"></i>
-                </button>
-                 <button class="delete">
-                <i class="far fa-trash-alt"></i>
-                </button>
-            <div>
-            
+  submitButton.addEventListener('click', () => {
+    if (newTodoInput.value.trim() !== '') {
+      todosContainer.innerHTML += `
+      <div class="todo">
+        <span class='itemTodo'>${newTodoInput.value}</span>
+        <div class="actions">
+          <button class="edit">
+            <i class="fas fa-edit"></i>
+          </button>
+          <button class="delete">
+            <i class="far fa-trash-alt"></i>
+          </button>
         </div>
-  `
-  }
+      </div>
+      `;
+      newTodoInput.value = '';
+      addEventListeners();
+    }
+  });
 
-  document.querySelectorAll('.delete').forEach(del=>{
-    del.addEventListener('click',()=>{
-      del.closest('.todo').remove()
-    })
-  })
- document.querySelectorAll('.edit').forEach(ed=>{
-  ed.addEventListener('click',()=>{
-    ed.closest('.todo').querySelector('.itemTodo').textContent=newTodoInput.value
-    newTodoInput.value=''
-  })
- })
+  for (let i = 0; i < response.todos.length; i++) {
+    todosContainer.innerHTML += `
+    <div class="todo">
+      <span class='itemTodo'>${response.todos[i].todo}</span>
+      <div class="actions">
+        <button class="edit">
+          <i class="fas fa-edit"></i>
+        </button>
+        <button class="delete">
+          <i class="far fa-trash-alt"></i>
+        </button>
+      </div>
+    </div>
+    `;
+  }
+  addEventListeners();
 }
 
-getTodos()
+function addEventListeners() {
+  document.querySelectorAll('.delete').forEach(del => {
+    del.onclick = () => {
+      del.closest('.todo').remove();
+    };
+  });
+  document.querySelectorAll('.edit').forEach(ed=>{
+      ed.addEventListener('click',()=>{
+        ed.closest('.todo').querySelector('.itemTodo').textContent=newTodoInput.value
+        newTodoInput.value=''
+      })
+     })
+  
+}
+
+getTodos();
+
+// 
+ 
